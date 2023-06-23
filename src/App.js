4a8ae66ebbde84e43fc07ce10d4a124140c591f4
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-06-23 18:46:12 +0400

    updates test 23

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>update 1</h1>
+      <h1>update 1</h1>
+      <h1>update 3</h1>
+    </div>
+  );
+}
+
+export default App;
